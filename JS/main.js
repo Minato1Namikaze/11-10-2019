4be9fec2b7e29fe310d1dad6d446d49c3e9e0448
Minato1Namikaze <halo_10_dead@gmail.com.mx>
2019-10-11 13:44:26 -0500

    subiendo otro archivo

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+let hambre = false
+
+if(hambre == true){
+
+    console.log("Estas triste")
+}
+
+else{
+
+    console.log("Estas feliz")
+}
+
+
+let precioDeDron = 16000
+
+let deudas = false
+ 
+
+let saldo = 1800
+
+if(saldo >= precioDeDron && !deudas){
+    console.log("Date")
+}
+
+else{
+    console.log("Ahorita no joven")
+}
